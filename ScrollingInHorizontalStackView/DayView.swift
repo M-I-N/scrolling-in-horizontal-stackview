f2c02db8c25be4cb916db8b9650be7b7e93470f0
Nayem <nil.nayem@gmail.com>
2018-04-24 01:14:08 +0600

    Scrolling in horizontal stack view done with resizable view.

---
+++ ScrollingInHorizontalStackView/DayView.swift
@@ -0,0 +1,23 @@
+//
+//  DayView.swift
+//  ScrollingInHorizontalStackView
+//
+//  Created by Nayem on 4/23/18.
+//  Copyright © 2018 Mufakkharul Islam Nayem. All rights reserved.
+//
+
+import UIKit
+
+class DayView: UIView {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var detailLabel: UILabel!
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
--- ScrollingInHorizontalStackView/ViewController.swift
+++ ScrollingInHorizontalStackView/ViewController.swift
@@ -9,10 +9,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var horizontallyScrollableStackView: UIStackView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        for _ in 0...10 {
+            if let dayView = Bundle.main.loadNibNamed("DayView", owner: nil, options: nil)!.first as? DayView {
+                dayView.translatesAutoresizingMaskIntoConstraints = false
+                dayView.widthAnchor.constraint(equalToConstant: horizontallyScrollableStackView.frame.height).isActive = true
+                dayView.titleLabel.text = "Friday"
+                dayView.detailLabel.text = "A long detail text will be shown here. A long detail text will be shown here. "
+                horizontallyScrollableStackView.addArrangedSubview(dayView)
+            }
+        }
     }
 
     override func didReceiveMemoryWarning() {
